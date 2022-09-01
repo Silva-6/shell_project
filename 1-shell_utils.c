b293b93e47cdd1677b1fad7dded201e5ba38cdba
Silva-6 <silva6media@gmail.com>
2022-09-01 01:42:57 +0000

    Prompt and Input/Read Implementation

--- 1-shell_utils.c
+++ 1-shell_utils.c
@@ -5,3 +5,57 @@ void ctrl_C(int signum)
 	if (signum == SIGINT)
 		print("\n ($) ", STDIN_FILENO);
 }
+
+/**
+ * _getline - takes input from the user
+ *
+ * Return: the string read
+ */
+
+char *_getline(void)
+{
+	int bufSize = READ_BUF, no_read, position = 0;
+
+	char *buffer = malloc(bufSize * sizeof(char));
+	char c;
+
+	if (buffer == NULL)
+	{
+		perror("Failed to allocate memory");
+		exit(EXIT_FAILURE);
+	}
+	while (1)
+	{
+		no_read = read(STDIN_FILENO, &c, 1);
+		if (c == EOF || !no_read)
+		{
+			/* checks if the input is EOT
+			 	(ctrl+D) and if it is from the terminal
+			*/
+			if (isatty(STDIN_FILENO) == 1)
+			{
+				print("\n", STDIN_FILENO);
+				return (NULL);
+			}
+		}
+		else if (c == '\n' || !no_read)
+		{
+			buffer[position] = '\0';
+			return (buffer); 
+		}
+		else
+			buffer[position] = c;
+		position++;
+
+		if (position >= bufSize)
+		{
+			bufSize += READ_BUF;
+			buffer = _realloc(buffer, READ_BUF, bufSize);
+			if (!buffer)
+			{
+				perror("Failed to re-allocate a space in the memory");
+				exit(EXIT_FAILURE);
+			}
+		}
+	}
+}
--- shell.h
+++ shell.h
@@ -7,10 +7,23 @@
 #include <signal.h>
 #include <unistd.h>
 
+/* initial size of buffer for user input */
+#define READ_BUF 1000
+
+
+/* shell utility functions */
 void ctrl_C(int);
+char *_getline(void);
 
 int print(char *, int);
 
 int _strlen(char *);
+
+/* shell string functions */
+int _strlen(char *);
+int _strcmp(char*, char *);
+
+/* shell memory management */
+void *_realloc(void *, int, int); 
 
 #endif /* SHELL_H */
